dd53cb9fcc2585b54311b1972fef25d8b7b66035
Lee Doughty <leedoughtydesign@gmail.com>
2019-02-09 11:10:34 +0000

    Add add todo function

--- src/App.js
+++ src/App.js
@@ -21,7 +21,13 @@ function TodoForm({addTodo}) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)}/>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder = "Add todo..."
+        onChange={e => setValue(e.target.value)}
+      />
     </form>
   )
 }
@@ -42,12 +48,18 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo}/>
         ))}
+        <TodoForm addTodo={addTodo}/>
       </div>
     </div>
   )
