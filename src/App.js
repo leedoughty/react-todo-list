6b8fd45f28c5bcee4938a6ed7e8871fe2d6b19ab
Lee Doughty <leedoughtydesign@gmail.com>
2019-02-09 11:03:02 +0000

    Add todo form function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ function Todo({ todo, index }) {
   )
 }
 
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
